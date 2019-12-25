948921e1958f4f0a23480c7fe4b39c8a91d9e165
LoL-CaT <W304088@yandex.ru>
2019-12-25 14:27:48 +0700

    15

---
+++ resource/texture/fx/_bmp/explosion/groundHit.s
@@ -0,0 +1,38 @@
+{bitmap
+	{Axis  120 60 }
+	{Filtering  1 }
+	{AlphaChannel  1 }
+	{Blend  Alpha }
+	{Animation  Play }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0000.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0001.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0002.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0003.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0004.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0005.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0006.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0007.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0008.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0009.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0010.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0011.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0012.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0013.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0014.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0015.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0016.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0017.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0018.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0019.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0020.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0021.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0022.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0023.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0024.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0025.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0026.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0028.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0029.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0030.tga" 45 }
+	{Frame  "dirtsplashlong.tga/dirtsplashdarklong0031.tga" 45 }	
+}
